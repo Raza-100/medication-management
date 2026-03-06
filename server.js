ed8065fce5972ecbe748de584a424ed9523e0c0f
Raza-100 <mdahamedraza18@gmail.com>
2026-03-06 22:21:32 +0530

    fix mysql railway connection

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 /* ---------------- DATABASE ---------------- */
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 /* ---------------- TEST ROUTE ---------------- */
